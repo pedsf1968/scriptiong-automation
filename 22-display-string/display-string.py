b3060a8cb12b85ec60b128c48c9f4c28a86551a8
DOS SANTOS FACAO Paul-Emmanuel <pedsf.fullstack@gmail.com>
2023-07-22 10:36:10 +0200

    Add the first use of module os

--- 22-display-string/display-string.py
+++ 22-display-string/display-string.py
@@ -1,9 +1,13 @@
-screen_width=int(input("Enter your screen width: "))
+# Imprt os to get size of terminal
+import os
+
+screen_width=os.get_terminal_size().columns
+
 given_str=input("Enter your string: ")
 print("Center string")
-print(given_str.center(screen_width))
+print(given_str.center(screen_width).title())
 print("Left justify")
-print(given_str.ljust(screen_width))
+print(given_str.ljust(screen_width).title())
 print("Right justify")
-print(given_str.rjust(screen_width))
+print(given_str.rjust(screen_width).title())
 
